0c6302fe73902d5ede20fa03bb08ecfbb1f86d51
Simone <simone.giacomelli@gmail.com>
2016-12-14 11:54:16 +0100

    enabled cors

--- src/main/java/com/penninkhof/odata/utils/CxfServletRegister.java
+++ src/main/java/com/penninkhof/odata/utils/CxfServletRegister.java
@@ -1,16 +1,21 @@
 package com.penninkhof.odata.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class CxfServletRegister {
-	
+
 	@Bean
 	public ServletRegistrationBean getODataServletRegistrationBean() {
 		ServletRegistrationBean odataServletRegistrationBean = new ServletRegistrationBean(new CXFNonSpringJaxrsServlet(), "/odata.svc/*");
@@ -20,4 +25,39 @@ public class CxfServletRegister {
 		odataServletRegistrationBean.setInitParameters(initParameters);
 		return odataServletRegistrationBean;
 	}
+
+	@Bean
+	public FilterRegistrationBean someFilterRegistration() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.addUrlPatterns("/odata.svc/*");
+		registration.setName("corsFilter");
+		registration.setOrder(1);
+		registration.setFilter(new Filter() {
+
+			@Override
+			public void doFilter(ServletRequest req, ServletResponse res,
+								 FilterChain chain) throws IOException, ServletException {
+				HttpServletResponse response = (HttpServletResponse) res;
+				HttpServletRequest request = (HttpServletRequest) req;
+				response.addHeader("Access-Control-Allow-Origin", "*");
+				response.addHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, MaxDataServiceVersion,sap-contextid-accept,x-csrf-token,DataServiceVersion,sap-cancel-on-close");
+				if ("OPTIONS".equals(request.getMethod())) {
+					response.setStatus(200);
+					response.setContentType("text/plain");
+				} else
+					chain.doFilter(req, res);
+
+			}
+
+			@Override
+			public void destroy() {
+			}
+
+			@Override
+			public void init(FilterConfig arg0) throws ServletException {
+			}
+		});
+		return registration;
+	}
+
 }
